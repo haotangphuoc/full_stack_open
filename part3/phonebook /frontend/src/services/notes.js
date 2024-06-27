5b70a1465419e97c0e33e638c6f1f440d2561e58
Hào Tăng <haotang@Haos-MacBook-Air.local>
2024-06-27 15:33:27 -0300

    Added exercices 3.9-3.11

---
+++ part3/phonebook /frontend/src/services/notes.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+const baseUrl = '/api/persons'
+
+const get = () => {
+    return axios.get(baseUrl)
+        .then(response => {
+            return response.data
+        })
+        .catch(error => {
+            console.log("Cant fetch data")
+        })
+}
+
+const create = (newPerson) => {
+    return axios.post(baseUrl, newPerson)
+        .then(response => {
+            return response.data
+        })
+        .catch(error => {
+            console.log("Cant create new person")
+        })
+}
+
+const deleteItem = (id) => {
+    return axios.delete(`${baseUrl}/${id}`)
+    .then(response => {
+        return response.data
+    })
+    .catch(error => {
+        console.log("Cant delete person")
+    })
+}
+
+const patch = (id, editedPerson) => {
+    return axios.patch(`${baseUrl}/${id}`, editedPerson)
+        .then(response => {
+            return response.data
+        })
+        .catch(error => {
+            console.log("Cant edit person")
+        })
+}
+export default {get, create, patch, deleteItem}
