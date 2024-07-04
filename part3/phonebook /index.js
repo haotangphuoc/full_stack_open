5188c3847618d1c37927656c145e145ab7449e61
haotangphuoc <tangphuoc.hao21@gmail.com>
2024-07-03 22:36:02 -0300

    Added exercice 3.22

--- part3/phonebook /index.js
+++ part3/phonebook /index.js
@@ -2,106 +2,106 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors());
+app.use(cors())
 app.use(express.static('dist'))
 
 const Person = require('./models/person')
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if(error.name == 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        console.log(error.name)
-        return response.status(400).send(error)
-    }
+  if(error.name == 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    console.log(error.name)
+    return response.status(400).send(error)
+  }
 
-    next(error)
+  next(error)
 }
 
-app.get('/api/persons', (request, response, next) => {
-    Person.find({})
-        .then(result => {
-            if(result) {
-                response.json(result)
-            }
-            else {
-                response.status(404).end()
-            }
-        })
+app.get('/api/persons', (request, response) => {
+  Person.find({})
+    .then(result => {
+      if(result) {
+        response.json(result)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
 })
 
 app.get('/info/', (request, response, next) => {
-    Person.find({})
-        .then(result => {
-            if(result) {
-                response.send(
-                    `<div>
-                        <p>Phonebook has info for ${persons.length}</p>   
-                        <p>${new Date()}</p>
-                     </div>`)
-            }
-            else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.find({})
+    .then(result => {
+      if(result) {
+        response.send(
+          `<div>
+            <p>Phonebook has info for ${result.length}</p>   
+            <p>${new Date()}</p>
+          </div>`)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
     
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
-    const objId = request.params.id
-    Person.findById(objId).then(result => {
-        response.json(result)
-    })
+app.get('/api/persons/:id', (request, response) => {
+  const objId = request.params.id
+  Person.findById(objId).then(result => {
+    response.json(result)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons/', (request, response, next) => {
-    const body = request.body
-    if(!body) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const newPerson = Person({
-        name: body.name,
-        number: body.number
+  const body = request.body
+  if(!body) {
+    return response.status(400).json({
+      error: 'content missing'
     })
-    newPerson.save()
-        .then(savedPerson => {
-        response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  }
+
+  const newPerson = Person({
+    name: body.name,
+    number: body.number
+  })
+  newPerson.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    if(!body) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-    const newPerson = {
-        name: body.name,
-        number: body.number
-    }
+  const body = request.body
+  if(!body) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const newPerson = {
+    name: body.name,
+    number: body.number
+  }
 
-    Person.findByIdAndUpdate(request.params.id, newPerson, { new: true })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, newPerson, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.use(errorHandler)
--- part3/phonebook /models/person.js
+++ part3/phonebook /models/person.js
@@ -5,39 +5,39 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 mongoose.connect(process.env.MONGO_URL)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('failed to connect to MongoDb', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('failed to connect to MongoDb', error.message)
+  })
 
 const personSchema = mongoose.Schema({
-    name: {
-        type: String, 
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String, 
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: function(v) {
-              // Regex to match xx-xxxxxxx or xxx-xxxxxxxx
-              return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number! Format should be xx-xxxxxxx or xxx-xxxxxxxx.`
-          }
-    },
+  name: {
+    type: String, 
+    minLength: 3,
+    required: true
+  },
+  number: {
+  	type: String, 
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+      // Regex to match xx-xxxxxxx or xxx-xxxxxxxx
+      	return /^\d{2,3}-\d+$/.test(v)
+    	},
+    	message: props => `${props.value} is not a valid phone number! Format should be xx-xxxxxxx or xxx-xxxxxxxx.`
+  	}
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
