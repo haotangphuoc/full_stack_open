392ef8a8e1422e91f8c2a7f0ba2fd8e72cca68bc
haotangphuoc <tangphuoc.hao21@gmail.com>
2024-07-12 22:28:47 -0300

    Added utils repository

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,16 +1,22 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const Blog = require('./models/blog')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
 
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
