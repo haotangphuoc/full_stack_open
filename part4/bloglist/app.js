0cc6ab6f6422b0465ad7db60424526b215f1450e
haotangphuoc <tangphuoc.hao21@gmail.com>
2024-07-15 13:41:17 -0300

    Added exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const Blog = require('./models/blog')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blog')
+const userRouter = require('./controllers/user')
 const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
