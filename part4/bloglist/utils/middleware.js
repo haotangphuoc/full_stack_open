e532451ce0a346ed218add45f40a9795e363c9c8
haotangphuoc <tangphuoc.hao21@gmail.com>
2024-07-14 21:45:48 -0300

    Added test env database

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -20,6 +20,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
+  
 
   next(error)
 }
